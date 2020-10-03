b8b7c94fc433ecfcde4fb0237161179c640c79b5
Hanna <svetlova.hanna@mail.ru>
2020-10-03 21:09:32 +0300

    feat: add solution for task typical-arrays-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0)
+    {
+        let minNumber = array[0];       
+    for ( let i = 0; i < array.length; ++i ) {
+
+            if ( minNumber > array[i] ) {
+                minNumber = array[i];
+            }
+        }
+    return minNumber;
+} else {
+    return 0;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0)
+    {
+        let maxNumber = array[0];       
+    for ( let i = 0; i < array.length; ++i ) {
+
+            if ( maxNumber < array[i] ) {
+                maxNumber = array[i];
+            }
+        }
+    return maxNumber;
+} else {
+    return 0;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let arraySum = 0;
+
+    if ( !(Array.isArray(array)) || array == null || array.length == null || array.length === 0 ) {
+        return 0;
+    }
+    
+    for ( let i = 0; i < array.length; i++ ) {
+        arraySum += array[i];
+    }
+
+    let i = array.length;
+    let arrayAvg = ( arraySum / i );
+    return arrayAvg;
+
 }
